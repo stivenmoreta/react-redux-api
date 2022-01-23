7ce7cf726689c08a82b5598a717114ba9095322e
enero70 <stivenmoreta.c@gmail.com>
2022-01-23 09:26:59 -0300

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle'
+import CantidadPokemon from './components/CantidadPokemon';
+import ComprarPokemon from './components/CompraPokemon';
+import store from './redux/store'
+import {Provider} from 'react-redux';
+import BuscadorPokemon from './components/buscador/BuscadorPokemon';
+import ResultadoPokemon from './components/buscador/ResultadoPokemon';
+
+
 
 function App() {
+
+  console.log(store.getState());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <div className='row'>
+          <div className='col-12'>
+            <div className='card mt-5' style={{maxWidth: '370px'}}>
+              <div className='row no-gutters'>
+                <div className='col-4'>
+                    <img src={require('./img/raiku.gif')} className="card-img" alt='pokemon'/>
+                </div>
+                <div className='col-8'>
+                  <div className='card-body'>
+                    <div className='card-title h3 text-center'>
+                      <CantidadPokemon/>
+                    </div>
+                    <ComprarPokemon/>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+          <div className='col-12 mt-4 border-top pt-3'>
+            <BuscadorPokemon/>
+          </div>
+          <div className='col-12 '>
+            <ResultadoPokemon/>
+          </div>
+        </div>
+      </div>
+    </Provider>
   );
 }
 
